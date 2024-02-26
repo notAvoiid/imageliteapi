ed9bf51a367f5bc44056d2bfdf40f6f9cf1d5444
Igor <igor.nacimento51@gmail.com>
2024-02-26 15:36:51 -0300

    "First commit"

---
+++ src/test/java/com/igor/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
