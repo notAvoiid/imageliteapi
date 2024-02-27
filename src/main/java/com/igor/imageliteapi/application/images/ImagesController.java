e726639bffa8eee838a61a27bc3d5e72f880a92f
Igor <igor.nacimento51@gmail.com>
2024-02-27 14:23:00 -0300

    "Creating URI and Mapper for the Image"

--- src/main/java/com/igor/imageliteapi/application/images/ImagesController.java
+++ src/main/java/com/igor/imageliteapi/application/images/ImagesController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,7 +25,7 @@ import java.util.List;
 public class ImagesController {
 
     private final ImageService service;
-
+    private final ImageMapper mapper;
     @PostMapping
     public ResponseEntity<Image> save(
             @RequestParam(value = "image_file", required = true) MultipartFile file,
@@ -32,17 +34,18 @@ public class ImagesController {
     ) throws IOException {
         log.info("Image received: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
 
-        Image image = Image.builder()
-                .name(name)
-                .tags(String.join(",", tags))
-                .size(file.getSize())
-                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
-                .file(file.getBytes())
-                .build();
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage = service.save(image);
+        URI imageUri = buildImageURL(savedImage);
 
-        service.save(image);
+        return ResponseEntity.created(imageUri).build();
+    }
 
-        return ResponseEntity.ok().build();
+    private URI buildImageURL(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path(imagePath).build().toUri();
     }
 
 }
